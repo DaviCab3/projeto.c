42fba37c43bde4545e0628a67f83bdc63c68685b
DaviCab3 <davicabeceira@gmail.com>
2025-09-10 10:18:46 -0300

    Update LISTA.h

--- LISTA.h
+++ LISTA.h
@@ -1,49 +1,39 @@
 #ifndef LISTA_H
 #define LISTA_H
 
-
 typedef struct Ator {
     char nome[50];
     struct Ator *ant;
     struct Ator *prox;
 } Ator;
 
-
 typedef struct Filme {
     char titulo[50];
     int ano;
     struct Filme *ant;
     struct Filme *prox;
-    Ator *atores;   // ponteiro para a lista de atores do filme
+    Ator *atores;
 } Filme;
 
-
-
+// ================== CRIAR ==================
 Filme* criarFilme(char *titulo, int ano);
 Ator* criarAtor(char *nome);
 
-
-
-int inserirFilme(Filme **listapor, Filme *novo);
+// ================== INSERIR ==================
+int inserirFilme(Filme **lista, Filme *novo);
 int inserirAtor(Filme *filme, Ator *novo);
 
-
-
-int exibirFilmes(Filme *lista);
+// ================== EXIBIR ==================
 int exibirAtores(Ator *lista);
+int exibirFilmes(Filme *lista);
 
-
-
+// ================== BUSCAR ==================
 Filme* buscarFilme(Filme *lista, char *titulo);
-Ator* buscarAtor(Ator *lista, char *nome);
 
-
-
-void removerFilme(Filme **lista, char *titulo);
-void removerAtor(Filme *filme, char *nome);
-
-
-
+// ================== ARQUIVO ==================
+int lerArquivoFilmes(const char *nomeArquivo, Filme **lista);
 
 #endif
+
+
 
